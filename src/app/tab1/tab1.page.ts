5ed0df39bfa5ab1e59a9d06dcd668ba7bd5c472a
vimalkumar015 <vimivimal@outlook.com>
2022-02-09 22:06:20 +0530

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import SwiperCore, { Navigation, Pagination, Virtual } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 
+SwiperCore.use([Pagination, Navigation, Virtual]);
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  slides: any;
+  @ViewChild("swiperRef", { static: false }) swiperRef?: SwiperComponent;
 
-  constructor() {}
+
+  constructor() {
+    this.slides = Array.from({ length: 7000 }).map((_, index) => {
+      var temp = { name: `Slide ${index + 1}`, price: 5 * index + 1 };
+      return temp;
+    });
+    console.log(this.slides);
+
+  }
 
 }
